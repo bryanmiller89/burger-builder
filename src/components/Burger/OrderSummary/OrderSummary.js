912f732b37c470585388d705f82d38bc22a84a91
Bryan Miller <bryanmiller89@gmail.com>
2018-07-09 16:49:19 -0700

    Make price show in USD

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,7 @@ const OrderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <strong><p>Total Price: ${props.price.toFixed(2)}</p></strong>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
