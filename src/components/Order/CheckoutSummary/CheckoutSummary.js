7af6508e457a86f3406a14d330c4e738b9144a0c
Bryan Miller <bryanmiller89@gmail.com>
2018-07-10 15:16:00 -0700

    Build out checkout summary component

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import classes from './CheckoutSummary.css';
+import Button from '../../UI/Button/Button';
+
+const CheckoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope is tastes well</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button
+        btnType="Danger"
+        clicked={props.checkoutCancelled}>
+        CANCEL
+      </Button>
+      <Button
+        btnType="Success"
+        clicked={props.checkoutContinued}>
+        CONTINUE
+      </Button>
+    </div>
+  );
+}
+
+export default CheckoutSummary;
