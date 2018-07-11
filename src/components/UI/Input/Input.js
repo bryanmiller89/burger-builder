f3417dc88ce4114ccf8b0b5ff594c39411f28814
Bryan Miller <bryanmiller89@gmail.com>
2018-07-11 12:58:58 -0700

    Add input and form validations

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import classes from './Input.css';
 
-const Input = (props) => {
+const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
 
   switch (props.elementType) {
     case ('input'):
-      inputElement = <input 
-        className={classes.inputElement} 
-        {...props.elementConfig} 
-        value={props.value} 
-        onChange={props.changed}/>
+      inputElement = <input
+        className={inputClasses.join(' ')}
+        {...props.elementConfig}
+        value={props.value}
+        onChange={props.changed} />;
       break;
     case ('textarea'):
-      inputElement = <textarea 
-        className={classes.inputElement}
+      inputElement = <textarea
+        className={inputClasses.join(' ')}
+        {...props.elementConfig}
         value={props.value}
-        onChange={props.changed} />
+        onChange={props.changed} />;
       break;
     case ('select'):
       inputElement = (
         <select
-          className={classes.inputElement}
+          className={inputClasses.join(' ')}
           value={props.value}
           onChange={props.changed}>
           {props.elementConfig.options.map(option => (
@@ -33,10 +39,11 @@ const Input = (props) => {
       );
       break;
     default:
-      inputElement = <input 
-        className={classes.inputElement} 
-        {...props.elementConfig} 
-        value={props.value} />
+      inputElement = <input
+        className={inputClasses.join(' ')}
+        {...props.elementConfig}
+        value={props.value}
+        onChange={props.changed} />;
   }
 
   return (
@@ -44,6 +51,8 @@ const Input = (props) => {
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
     </div>
-  )
-}
-export default Input;
+  );
+
+};
+
+export default input;
